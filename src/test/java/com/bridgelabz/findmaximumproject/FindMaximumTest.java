71c79ca8c319a7f4c0bc03b7fd601ba2e908c7c3
vaishnavipardeshi <vaishnavipardeshi22@gmail.com>
2020-03-14 15:54:01 +0530

    ADDED : Test 2 to check maximum integer value at middle position

--- src/test/java/com/bridgelabz/findmaximumproject/FindMaximumTest.java
+++ src/test/java/com/bridgelabz/findmaximumproject/FindMaximumTest.java
@@ -11,4 +11,11 @@ public class FindMaximumTest {
         Integer maximumValue = findMaximum.getMaximumInteget(10,9,8);
         Assert.assertEquals((Integer) 10, maximumValue);
     }
+
+    @Test
+    public void givenThreeIntegers_WhenMaximumAtMiddle_ReturnMaximumValue() {
+        FindMaximum findMaximum = new FindMaximum();
+        Integer maximumValue = findMaximum.getMaximumInteget(8,10,9);
+        Assert.assertEquals((Integer) 10, maximumValue);
+    }
 }
