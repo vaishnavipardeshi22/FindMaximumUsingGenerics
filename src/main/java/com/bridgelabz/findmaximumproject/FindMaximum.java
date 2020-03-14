015d4f85e844e5b1d39738e0055a86f70c6ee878
vaishnavipardeshi <vaishnavipardeshi22@gmail.com>
2020-03-14 15:44:22 +0530

    ADDED : Function to get maximum integer value from three integers

--- src/main/java/com/bridgelabz/findmaximumproject/FindMaximum.java
+++ src/main/java/com/bridgelabz/findmaximumproject/FindMaximum.java
@@ -5,4 +5,12 @@ public class FindMaximum {
     public static void main(String[] args) {
         System.out.println("******************** WELCOME TO FIND MAXIMUM ********************");
     }
+
+    //METHOD TO GET MAXIMUM INTEGER VALUE
+    public Integer getMaximumInteget(Integer firstValue, Integer secondValue, Integer thirdValue) {
+        Integer maximum=firstValue;
+        Integer maximumInteger = thirdValue > (secondValue.compareTo(maximum) > 0 ? secondValue : firstValue) ? thirdValue :
+                ((secondValue.compareTo(maximum) > 0) ? secondValue : firstValue);
+        return maximumInteger;
+    }
 }
