7fd4feeeddb035b8f448537ba65fdad85ee6c569
vaishnavipardeshi <vaishnavipardeshi22@gmail.com>
2020-03-14 17:41:33 +0530

    ADDED : Function to get maximum string value from three strings

--- src/main/java/com/bridgelabz/findmaximumproject/FindMaximum.java
+++ src/main/java/com/bridgelabz/findmaximumproject/FindMaximum.java
@@ -14,10 +14,21 @@ public class FindMaximum {
         return maximumInteger;
     }
 
+    //METHOD TO GET MAXIMUM FLOAT VALUE
     public Float getMaximumFloat(Float firstValue, Float secondValue, Float thirdValue) {
         Float maximum=firstValue;
         Float maximumFloat = thirdValue > (secondValue.compareTo(maximum) > 0 ? secondValue : firstValue) ? thirdValue :
                 ((secondValue.compareTo(maximum) > 0) ? secondValue : firstValue);
         return maximumFloat;
+    }
+
+    //METHOD TO GET MAXIMUM STRING VALUE
+    public String getMaximumString(String firstValue, String secondValue, String thirdValue) {
+        String maximum=firstValue;
+        if (secondValue.compareTo(maximum) > 0)
+            maximum = secondValue;
+        if (thirdValue.compareTo(maximum) > 0)
+            maximum = thirdValue;
+        return maximum;
     }
 }
