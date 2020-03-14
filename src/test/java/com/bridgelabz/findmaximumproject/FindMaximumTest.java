49577816288dbe89f13b997434f93d100f994e55
vaishnavipardeshi <vaishnavipardeshi22@gmail.com>
2020-03-14 15:42:26 +0530

    ADDED : Test 1 to check maximum value at first position

--- src/test/java/com/bridgelabz/findmaximumproject/FindMaximumTest.java
+++ src/test/java/com/bridgelabz/findmaximumproject/FindMaximumTest.java
@@ -1,4 +1,14 @@
 package com.bridgelabz.findmaximumproject;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class FindMaximumTest {
+
+    @Test
+    public void givenThreeIntegers_WhenMaximumAtStart_ReturnMaximumValue() {
+        FindMaximum findMaximum = new FindMaximum();
+        Integer maximumValue = findMaximum.getMaximumInteget(10,9,8);
+        Assert.assertEquals((Integer) 10, maximumValue);
+    }
 }
